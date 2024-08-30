536885a2f449297ca260c64b7eb8c9ca9e91f48a
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 19:41:04 +0200

    add ProductGrid component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,15 @@
-import { titleFont } from '@/config/fonts';
+import { ProductGrid } from '@/components';
 import { Title } from '@/components/ui/title/Title';
+import { initialData } from '@/seed/seed';
+
+const products = initialData.products;
 
 export default function ShopPage() {
   return (
     <>
       <Title title='Shop' subtitle='All products' className='mb-2' />
+
+      <ProductGrid products={products} />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+export * from './products/product-grid/ProductGrid';
+
 export * from './ui/top-menu/TopMenu';
 export * from './ui/title/Title';
