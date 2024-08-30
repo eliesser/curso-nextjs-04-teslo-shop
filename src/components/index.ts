40afcfc392c45b73a9732ce44d18a41124bd88f0
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 23:41:29 +0200

    add SizeSelector component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 
+export * from './product/size-selector/SizeSelector';
+
 export * from './ui/top-menu/TopMenu';
 export * from './ui/title/Title';
 export * from './ui/sidebar/Sidebar';
