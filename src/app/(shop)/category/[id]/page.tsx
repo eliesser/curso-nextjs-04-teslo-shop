e5a66d01b448091beaf9155b126732162ce027a0
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 19:21:48 +0200

    add PageNotFound component

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,14 @@
-export default function CategoryPage() {
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: { id: string };
+}
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+  if (id === 'kids') notFound();
+
   return (
     <div className=''>
       <h1>Category Page</h1>
---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,6 @@
+import { PageNotFound } from '@/components/ui/not-found/PageNotFound';
+import Link from 'next/link';
+
+export default function NotFount() {
+  return <PageNotFound />;
+}
