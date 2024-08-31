15587fce32500ed9b168d3461f267d855e0e048f
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-31 15:45:19 +0200

    fix styles

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -38,18 +38,18 @@ export const Pagination = ({ totalPages }: Props) => {
   return (
     <div className='flex text-center justify-center mt-10 mb-32'>
       <nav aria-label='Page navigation example'>
-        <ul className='flex list-style-none'>
-          <li className='page-item'>
+        <ul className='flex list-style-none gap-2'>
+          <li className='page-item flex items-center justify-center'>
             <Link
-              className='page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
+              className='page-link relative block py-2 px-2 bg-transparent border-0  outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
               href={createPageUrl(currentPage - 1)}
             >
-              <IoChevronBackOutline size={30} />
+              <IoChevronBackOutline size={20} />
             </Link>
           </li>
 
           {allPages.map((page, index) => (
-            <li key={page} className='page-item'>
+            <li key={page} className='page-item flex items-center justify-center'>
               <Link
                 className={clsx(
                   'page-link relative block py-1.5 px-3 border-0 outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none',
@@ -65,12 +65,12 @@ export const Pagination = ({ totalPages }: Props) => {
             </li>
           ))}
 
-          <li className='page-item'>
+          <li className='page-item flex items-center justify-center'>
             <Link
-              className='page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
+              className='page-link relative block py-2 px-2 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
               href={createPageUrl(currentPage + 1)}
             >
-              <IoChevronForwardOutline size={30} />
+              <IoChevronForwardOutline size={20} />
             </Link>
           </li>
         </ul>
