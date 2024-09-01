958cb6f2ec65b593cfc5d38036c084f83346aa4f
Eliesser Freites <freiteseliesser@hotmail.com>
2024-09-01 16:31:11 +0200

    register

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 export * from './auth/login';
 export * from './auth/logout';
+export * from './auth/register';
 
 export { getProductBySlug } from './product/get-product-by-slug';
 export { getStockBySlug } from './product/get-stock-by-slug';
